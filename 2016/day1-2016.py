ec2cb292ab87dbf74ec8600307c9aa8d3c72910a
skmllns <skmllns@gmail.com>
2016-12-01 15:27:05 -0500

    Day 1 2016, p2 complete (you should refactor this)

--- 2016/day1-2016.py
+++ 2016/day1-2016.py
@@ -1,41 +1,80 @@
-# dirs are starting at n, going clockwise
-dirs = ['n', 'e', 's', 'w']
-facingDir = 0
-firstTwiceVisted = ()
-foundFTV = bool(0)
+def readSteps(fileName):
+    with open(fileName, 'r') as f:
+        data = f.read()
+        steps = data.replace(',', '').split(" ")
+    return steps
 
-x = 0
-y = 0
-coords = [(0, 0)]
-timesVisited = {}
-
-with open('day1-2016.txt', 'r') as f:
-    data = f.read()
-    steps = data.replace(',', '').split(" ")
-
-for step in steps:
-    rotateDir = step[0]
-    stepNum = int(step[1:])
+def rotate(rotateDir, facingDir):
     if rotateDir == 'L':
         facingDir -= 1
     else:
         facingDir += 1
     facingDir %= 4
-    if dirs[facingDir] == 'n':
-        y += stepNum
-    elif dirs[facingDir] == 's':
-        y -= stepNum
-    elif dirs[facingDir] == 'w':
-        x -= stepNum
-    elif dirs[facingDir] == 'e':
-        x += stepNum
-    location = (x, y)
-    coords.append(location)
-
-firstDistance = abs(location[0]) + abs(location[1])
-print(firstDistance)
+    return facingDir
 
 
+def main():
+
+    # dirs are starting at n, going clockwise
+    dirs = 'nesw'
+    multipliers = [1, 1, -1, -1]
+
+    facingDir = 0
+    firstTwiceVisted = ()
+    foundFTV = bool(0)
+
+    x = 0
+    y = 0
+    coords = [(0, 0)]
+    passedThrough = []
+
+    steps = readSteps("day1-2016.txt")
+    for step in steps:
+        rotateDir = step[0]
+        stepNum = int(step[1:])
+        facingDir = rotate(rotateDir, facingDir)
+        scalar = multipliers[facingDir]
+
+        if foundFTV == bool(0):
+            if dirs[facingDir] in "ns":
+                for yAdd in range(1, stepNum + 1):
+                    yCoord = y + (yAdd * scalar)
+                    passedLocation = (x, yCoord)
+                    if passedLocation in passedThrough:
+                        firstTwiceVisted = passedLocation
+                        foundFTV = bool(1)
+                    else:
+                        passedThrough.append(passedLocation)
+
+            else:
+                for xAdd in range(1, stepNum + 1):
+                    xCoord = x + (xAdd * scalar)
+                    passedLocation = (xCoord, y)
+                    if passedLocation in passedThrough:
+                        firstTwiceVisted = passedLocation
+                        foundFTV = bool(1)
+                    else:
+                        passedThrough.append(passedLocation)
+
+        if dirs[facingDir] == 'n':
+            y += stepNum
+        elif dirs[facingDir] == 's':
+            y -= stepNum
+        elif dirs[facingDir] == 'w':
+            x -= stepNum
+        elif dirs[facingDir] == 'e':
+            x += stepNum
+        location = (x, y)
+
+
+
+
+    firstDistance = abs(location[0]) + abs(location[1])
+    secondDistance = abs(firstTwiceVisted[0]) + abs(firstTwiceVisted[1])
+    print(firstDistance)
+    print(secondDistance)
+
+main()
 
 
 
