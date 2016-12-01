d85372a9124333ebbd8846b5f3b6a92a71ff8624
skmllns <skmllns@gmail.com>
2016-12-01 17:04:43 -0500

    Day2-2015 is broken (sorry), but I was refactoring in order to add p2

--- 2015/day2-2015.py
+++ 2015/day2-2015.py
@@ -1,33 +1,46 @@
 # 2*l*w + 2*w*h + 2*h*l + (area of smallest side)
 import itertools
 
-totalFeet = 0
-measures = ['l', 'w', 'h']
-combos = list(itertools.combinations(measures, 2))
+def getPaperFootage(line, combos):
 
-with open('day2-2015.txt', 'r') as f:
-    for line in f:
-        if line == "\n":
-            break
-        else:
-            minArea = 9999999999999999999999
-            boxFeet = 0
+    minArea = 9999999999999999999999
+    boxFeet = 0
 
-            charElems = line.rstrip().split('x')
-            intElems = [int(e) for e in charElems]
-            elemDict = {"l": intElems[0], "w": intElems[1], "h": intElems[2]}
+    charElems = line.rstrip().split('x')
+    intElems = [int(e) for e in charElems]
+    elemDict = {"l": intElems[0], "w": intElems[1], "h": intElems[2]}
 
-            for comb in combos:
-                area = elemDict[comb[0]] * elemDict[comb[1]]
-                if area < minArea:
-                    minArea = area
+    for comb in combos:
+        area = elemDict[comb[0]] * elemDict[comb[1]]
+        if area < minArea:
+            minArea = area
 
-            boxFeet = 2*(elemDict["l"]*elemDict["w"] + elemDict["w"]*elemDict["h"] + elemDict["h"]*elemDict["l"]) + \
-                      minArea
+    boxFeet = 2 * (elemDict["l"] * elemDict["w"] + elemDict["w"] * elemDict["h"] + elemDict["h"] * elemDict["l"]) + \
+              minArea
 
-            totalFeet += boxFeet
+    return boxFeet
+#
+# def getRibbonFootage(line, combos):
 
-print(totalFeet)
 
+def main():
+    totalPaperFootage = 0
+
+    with open('day2-2015.txt', 'r') as f:
+
+        measures = ['l', 'w', 'h']
+        combos = list(itertools.combinations(measures, 2))
+
+        for line in f:
+            if line == "\n":
+                break
+            else:
+                totalPaperFootage += getPaperFootage(line, combos)
+
+
+    print(totalPaperFootage)
+
+
+main()
 
 
