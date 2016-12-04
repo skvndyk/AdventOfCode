f49b242361647f03c7ceb83ef13961a9d87de5de
skmullins <skmllns@gmail.com>
2016-12-04 11:33:30 -0500

    Day3-2016 part 2

--- 2016/day3-2016.py
+++ 2016/day3-2016.py
@@ -1,4 +1,4 @@
-def rowTest(sides):
+def sideTest(sides):
     x = 1
     for idx in range(0, 3):
         thirdSide = sides[idx]
@@ -10,15 +10,27 @@ def rowTest(sides):
     return x
 
 if __name__ == "__main__":
-    numPossible = 0
+    numPossibleRow = 0
+    numPossibleCol = 0
+
+    allRows = []
+    allColumns = []
 
     with open('day3-2016.txt', 'r') as f:
         for line in f:
-
             sides = line.strip().split("  ")
             sides = [s for s in sides if len(s) > 0]
             sides = [int(s.strip()) for s in sides]
-            numPossible += rowTest(sides)
+            allRows.append(sides)
+            numPossibleRow += sideTest(sides)
 
-    print("Part 1: {}".format(numPossible))
-    
+    allColumns = list(zip(*allRows))
+    for col in allColumns:
+        idx = 0
+        while (idx + 3) <= len(col):
+            sides = col[idx:idx+3]
+            numPossibleCol += sideTest(sides)
+            idx += 3
+
+    print("Part 1: {}".format(numPossibleRow))
+    print("Part 2: {}".format(numPossibleCol))
