56b11cca87ddcf923e7465c029175074644fd669
skmllns <skmllns@gmail.com>
2016-12-07 11:13:06 -0500

    Refactored Day7-2016 Pt1 (outside_brackets => supernets)

--- 2016/day7-2016.py
+++ 2016/day7-2016.py
@@ -2,9 +2,9 @@ import re
 
 def getParts(ip):
     hypernet = re.findall('(?<=\[)[a-z]+(?=\])', ip)
-    outside_brackets = re.findall('([a-z]+(?=\[))|((?<=\])[a-z]+)', ip)
-    outside_brackets = [x for y in outside_brackets for x in y if x != '']
-    return hypernet, outside_brackets
+    supernets = re.findall('([a-z]+(?=\[))|((?<=\])[a-z]+)', ip)
+    supernets = [x for y in supernets for x in y if x != '']
+    return hypernet, supernets
 
 def abbaTest(to_test):
     abbaFormat = False
@@ -22,27 +22,27 @@ if __name__ == "__main__":
     with open('day7-2016.txt', 'r') as f:
         valid_ips = 0
         for ip in f:
-            hypernet, outside_brackets = getParts(ip)
+            hypernets, supernets = getParts(ip)
 
-            num_outside_brackets = len(outside_brackets)
-            num_hypernets = len(hypernet)
+            num_supernets = len(supernets)
+            num_hypernets = len(hypernets)
 
             valid_hypernets = 0
-            valid_outside_brackets = 0
+            valid_supernets = 0
 
-            #hypernet should not be True, outside_brackets should be True
-            for item in hypernet:
+            #hypernet should not be True, supernet should be True
+            for item in hypernets:
                 if abbaTest(item) is False:
                     valid_hypernets += 1
                 else:
                     break
 
-            for item in outside_brackets:
+            for item in supernets:
                 if abbaTest(item) is True:
-                    valid_outside_brackets += 1
+                    valid_supernets += 1
                     break
 
-            if (valid_outside_brackets > 0) and (valid_hypernets == num_hypernets):
+            if (valid_supernets > 0) and (valid_hypernets == num_hypernets):
                 valid_ips += 1
 
 
@@ -52,7 +52,7 @@ if __name__ == "__main__":
 
 # **Explanation of list comprehension
 # bloop = []
-# for y in outside_brackets:
+# for y in supernets:
 #     for x in y:
 #         if x != '':
 #             bloop.append(x)
