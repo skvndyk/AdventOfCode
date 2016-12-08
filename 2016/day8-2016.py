4e724d62078cf2cd836105c4b71b2e34a948a047
skmllns <skmllns@gmail.com>
2016-12-08 10:37:02 -0500

    Day8-2016 Part 1, added test file

---
+++ 2016/day8-2016.py
@@ -0,0 +1,60 @@
+# dim: 50 wide x 6 tall
+from collections import OrderedDict
+
+def makerectangle(dimensions, grid):
+    stripped_dimensions = dimensions.split('x')
+    width = int(stripped_dimensions[0])
+    height = int(stripped_dimensions[1])
+    for w in range(width):
+        for h in range(height):
+            grid[(w, h)] = '#'
+    return grid
+
+
+def shiftpixels(parameters, grid, width, height):
+    group_type = parameters[0]
+    axis_num = int(parameters[1].split("=")[1])
+    num_to_shift = int(parameters[3])
+    new_grid = grid.copy()
+    if group_type == 'row':
+        for x in range(width):
+            move_x_pos = (x + num_to_shift) % width
+            new_grid[(move_x_pos, axis_num)] = grid[(x, axis_num)]
+    else:
+        for y in range(height):
+            move_y_pos = (y + num_to_shift) % height
+            new_grid[(axis_num, move_y_pos)] = grid[(axis_num, y)]
+    return new_grid
+
+def makegrid(width, height):
+    return OrderedDict.fromkeys([(x, y) for x in range(0, width) for y in range(0, height)], '.')
+
+
+def printgrid(grid, width, height):
+    for h in range(height):
+        print("\n")
+        for w in range(width):
+            print(grid[w, h], end='')
+    print('\n')
+
+
+
+if __name__ == "__main__":
+    lit_pixels = 0
+    with open('day8-2016.txt', 'r') as f:
+        width = 50
+        height = 6
+        grid = makegrid(width, height)
+        for line in f:
+            instructions = line.rstrip().split(" ")
+            if instructions[0] == 'rect':
+                grid = makerectangle(instructions[1], grid)
+                # printgrid(grid, width, height)
+            else:
+                grid = shiftpixels(instructions[1:5], grid, width, height)
+                # printgrid(grid, width, height)
+    for value in grid.items():
+        if value[1] == '#':
+            lit_pixels += 1
+
+    print(lit_pixels)
