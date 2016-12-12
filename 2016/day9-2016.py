132f9ebdefa37cf4c5183cb16ba2db45a9d81f8d
skmllns <skmllns@gmail.com>
2016-12-12 12:56:39 -0500

    Day9-2016 Pt1 works for first 3 test cases

--- 2016/day9-2016.py
+++ 2016/day9-2016.py
@@ -1,9 +1,7 @@
-import re
-
 def analyze_marker(marker):
     stripped_marker = marker.split('x')
-    num_chars = stripped_marker[0]
-    num_repeats = stripped_marker[1]
+    num_chars = int(stripped_marker[0])
+    num_repeats = int(stripped_marker[1])
     return num_chars, num_repeats
 
 if __name__ == "__main__":
@@ -12,21 +10,35 @@ if __name__ == "__main__":
         marker = ""
         data = f.read()
         in_marker = False
-        data = "A(1x5)BC"
-
-        for char in data:
-            if not in_marker:
-                if char != "(":
-                    decompressed += char
+        marker_exists = False
+        data = "(3x3)XYZ"
+        len_data = len(data)
+        x = 0
+        while x < (len_data - 1):
+            if not marker_exists:
+                curr_char = data[x]
+                if not in_marker:
+                    if curr_char != "(":
+                        decompressed += curr_char
+                    else:
+                        in_marker = True
                 else:
-                    in_marker = True
+                    if curr_char != ")":
+                        marker += curr_char
+                    else:
+                        in_marker = False
+                        marker_exists = True
+                        chars, repeats = analyze_marker(marker)
+                x += 1
             else:
-                if char != ")":
-                    marker += char
-                else:
-                    in_marker = False
-                    chars, repeats = analyze_marker(marker)
-                    x = 0
-
-
+                to_repeat = ""
+                for y in range(chars):
+                    pos = x + y
+                    to_repeat += data[pos]
+                for z in range(repeats):
+                    decompressed += to_repeat
+                marker = ""
+                marker_exists = False
+                x = pos
+    print(decompressed)
 
