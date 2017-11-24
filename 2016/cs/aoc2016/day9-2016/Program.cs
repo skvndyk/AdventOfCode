893f02a96d95267b443e5821d789eb0416a33944
skmullins <skmllns@gmail.com>
2017-11-24 14:40:44 -0500

    Added part 1 of day9-2016 in c#

---
+++ 2016/cs/aoc2016/day9-2016/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = System.IO.File.ReadAllText("day9-2016.txt");
+            string results = getDecompressed(input);
+            Console.WriteLine(results.Length);
+            Console.ReadLine();
+        }
+
+        public static string getDecompressed(string input)
+        {
+            bool inMarker = false;
+            bool markerExists = false;
+
+            int x = 0;
+            int lenData = input.Length;
+            int charNum = 0;
+            int repeatNum = 0;
+            int pos = 0;
+
+            char currChar;
+
+            string marker = "";
+            string decompressed = "";
+            string toRepeat = "";
+
+            Tuple<int, int> markerTuple;
+
+            while (x < lenData)
+            {
+                if (!markerExists)
+                {
+                    currChar = input[x];
+                    if (!inMarker)
+                    {
+                        if (currChar != '(')
+                        {
+                            decompressed += currChar;
+                        }
+                        else
+                        {
+                            inMarker = true;
+                        }
+                    }
+                    else
+                    {
+                        if (currChar != ')')
+                        {
+                            marker += currChar;
+                        }
+                        else
+                        {
+                            inMarker = false;
+                            markerExists = true;
+                            markerTuple = analyzeMarker(marker);
+                            charNum = markerTuple.Item1;
+                            repeatNum = markerTuple.Item2; 
+                        }
+                    }
+                    x += 1;
+                }
+                else
+                {
+                    toRepeat = "";
+                    for (int i = 0; i < charNum; i++)
+                    {
+                        pos = x + i;
+                        toRepeat += input[pos];
+                    }
+                    for (int j = 0; j < repeatNum; j++)
+                    {
+                        decompressed += toRepeat;
+                    }
+                    marker = "";
+                    markerExists = false;
+                    x = pos + 1;
+                }
+            }
+            return decompressed;
+        }
+
+        public static Tuple<int, int> analyzeMarker(string marker)
+        {
+            string[] strippedMarker = marker.Split('x');
+            int numChars = Int32.Parse(strippedMarker[0].ToString());
+            int numRepeats = Int32.Parse(strippedMarker[1].ToString());
+            return new Tuple<int, int>(numChars, numRepeats);
+        }
+    }
+}
---
+++ 2016/cs/aoc2016/day9-tests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day9;
+
+namespace Day9Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCase1()
+        {
+            string input = "A(1x5)BC";
+            string results = Day9.Program.getDecompressed(input);
+            Assert.AreEqual(results, "ABBBBBC");
+        }
+
+        [TestMethod]
+        public void TestCase2()
+        {
+            string input = "(3x3)XYZ";
+            string results = Day9.Program.getDecompressed(input);
+            Assert.AreEqual(results, "XYZXYZXYZ");
+        }
+
+        [TestMethod]
+        public void TestCase3()
+        {
+            string input = "A(2x2)BCD(2x2)EFG";
+            string results = Day9.Program.getDecompressed(input);
+            Assert.AreEqual(results, "ABCBCDEFEFG");
+        }
+
+        [TestMethod]
+        public void TestCase4()
+        {
+            string input = "(6x1)(1x3)A";
+            string results = Day9.Program.getDecompressed(input);
+            Assert.AreEqual(results, "(1x3)A");
+        }
+
+        [TestMethod]
+        public void TestCase5()
+        {
+            string input = "X(8x2)(3x3)ABCY";
+            string results = Day9.Program.getDecompressed(input);
+            Assert.AreEqual(results, "X(3x3)ABC(3x3)ABCY");
+        }
+    }
+}
