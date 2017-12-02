a26d41cea2113de677eb03943ff3465eb0bb61da
skmullins <skmllns@gmail.com>
2017-12-02 13:44:40 -0500

    finished day2-2017

--- 2017/cs/aoc-2017/Day2/Program.cs
+++ 2017/cs/aoc-2017/Day2/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace Day2
 {
-    class Program
+    public class Program
     {   
         static void Main(string[] args)
         {
+            string filePath = "day2-2017.txt";
+            Console.WriteLine(Part1(filePath));
+            //Part2(filePath);
+            Console.ReadLine();
+        }
+
+        public static int Part1(string filePath)
+        {
+            int sum = 0;
+            List<List<string>> splitLines = ReadTextIntoLists(filePath);
+            foreach (List<string> row in splitLines)
+            {
+                List<int> intRow = row.ConvertAll(r => Int32.Parse(r)).ToList();
+                int highVal = intRow.Max();
+                int lowVal = intRow.Min();
+                int diff = highVal - lowVal;
+                sum += diff;
+            }
+            return sum;
+        }
+        public static void Part2(string filePat)
+        {
+
+        }
+        public static List<List<string>> ReadTextIntoLists(string filePath)
+        {
+            string rawInput = System.IO.File.ReadAllText(filePath);
+            List<string> lines = rawInput.Split('\n').ToList();
+            List<List<string>> splitLines = new List<List<string>>();
+            foreach (string line in lines)
+            {
+                splitLines.Add(line.Split('\t').ToList());
+            }
+            return splitLines;
         }
     }
 }
---
+++ 2017/cs/aoc-2017/UnitTestProject2/Day2Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day2;
+
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class Day2Tests
+    {
+        [TestMethod]
+        public void Day2_P1()
+        {
+            string filePath = "day2p1-2017.txt";
+            int results = Day2.Program.Part1(filePath);
+            Assert.AreEqual(18, results);
+        }
+
+        [TestMethod]
+        public void Day2_P2()
+        {
+        }
+    }
+}
