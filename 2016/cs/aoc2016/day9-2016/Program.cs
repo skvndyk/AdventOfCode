1976d300f2d31f5a51ecbc1f274881f0aa369faa
skmullins <skmllns@gmail.com>
2017-12-02 12:32:37 -0500

    working on day9p2-2016

--- 2016/cs/aoc2016/day9-2016/Program.cs
+++ 2016/cs/aoc2016/day9-2016/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text.RegularExpressions;
 
 namespace Day9
 {
@@ -10,12 +11,12 @@ namespace Day9
         static void Main(string[] args)
         {
             string input = System.IO.File.ReadAllText("day9-2016.txt");
-            string results = getDecompressed(input);
+            string results = getDecompressed2(input);
             Console.WriteLine(results.Length);
             Console.ReadLine();
         }
 
-        public static string getDecompressed(string input)
+        public static string getDecompressed1(string input)
         {
             bool inMarker = false;
             bool markerExists = false;
@@ -33,6 +34,7 @@ namespace Day9
             string toRepeat = "";
 
             Tuple<int, int> markerTuple;
+
 
             while (x < lenData)
             {
@@ -62,7 +64,7 @@ namespace Day9
                             markerExists = true;
                             markerTuple = analyzeMarker(marker);
                             charNum = markerTuple.Item1;
-                            repeatNum = markerTuple.Item2; 
+                            repeatNum = markerTuple.Item2;
                         }
                     }
                     x += 1;
@@ -84,6 +86,104 @@ namespace Day9
                     x = pos + 1;
                 }
             }
+            return decompressed;
+        }
+
+
+        public static string getDecompressed2(string input)
+        {
+            bool inMarker = false;
+            bool markerExists = false;
+            bool allDecompressed = true;
+
+            int x = 0;
+            int lenData = input.Length;
+            int charNum = 0;
+            int repeatNum = 0;
+            int pos = 0;
+            int holdingPos = 0;
+
+            char currChar;
+
+            string marker = "";
+            string decompressed = "";
+            string toRepeat = "";
+            string toProcess = input;
+
+            Tuple<int, int> markerTuple;
+
+            string pattern = @".*(d*xd*).*";
+            Regex regex = new Regex(pattern);
+
+            do
+            {
+                if (x == lenData && !allDecompressed)
+                {
+                    x = 0;
+                    toProcess = decompressed;
+                    lenData = toProcess.Length;
+                    decompressed = "";
+                    allDecompressed = true;
+                }
+                while (x < lenData)
+                {
+                    if (!markerExists)
+                    {
+                        currChar = toProcess[x];
+                        if (!inMarker)
+                        {
+                            if (currChar != '(')
+                            {
+                                decompressed += currChar;
+                            }
+                            else
+                            {
+                                holdingPos = x;
+                                inMarker = true;
+                                allDecompressed = false;
+                            }
+                        }
+                        else
+                        {
+                            if (currChar != ')')
+                            {
+                                marker += currChar;
+                            }
+                            else
+                            {
+                                inMarker = false;
+                                markerExists = true;
+                                markerTuple = analyzeMarker(marker);
+                                charNum = markerTuple.Item1;
+                                repeatNum = markerTuple.Item2;
+                            }
+                        }
+                        x += 1;
+                    }
+                    else
+                    {
+                        toRepeat = "";
+                        for (int i = 0; i < charNum; i++)
+                        {
+                            pos = x + i;
+                            toRepeat += toProcess[pos];
+                        }
+                        for (int j = 0; j < repeatNum; j++)
+                        {
+                            decompressed += toRepeat;
+                        }
+                        marker = "";
+                        markerExists = false;
+                        x = pos + 1;
+                    }
+                }
+                if (regex.Matches(decompressed).Count == 0)
+                {
+                    allDecompressed = true;
+                }
+
+            } while (!allDecompressed);
+
             return decompressed;
         }
 
--- 2016/cs/aoc2016/day9-tests/UnitTest1.cs
+++ 2016/cs/aoc2016/day9-tests/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace Day9Tests
         public void TestCase1()
         {
             string input = "A(1x5)BC";
-            string results = Day9.Program.getDecompressed(input);
+            string results = Day9.Program.getDecompressed1(input);
             Assert.AreEqual(results, "ABBBBBC");
         }
 
@@ -19,7 +19,7 @@ namespace Day9Tests
         public void TestCase2()
         {
             string input = "(3x3)XYZ";
-            string results = Day9.Program.getDecompressed(input);
+            string results = Day9.Program.getDecompressed1(input);
             Assert.AreEqual(results, "XYZXYZXYZ");
         }
 
@@ -27,7 +27,7 @@ namespace Day9Tests
         public void TestCase3()
         {
             string input = "A(2x2)BCD(2x2)EFG";
-            string results = Day9.Program.getDecompressed(input);
+            string results = Day9.Program.getDecompressed1(input);
             Assert.AreEqual(results, "ABCBCDEFEFG");
         }
 
@@ -35,7 +35,7 @@ namespace Day9Tests
         public void TestCase4()
         {
             string input = "(6x1)(1x3)A";
-            string results = Day9.Program.getDecompressed(input);
+            string results = Day9.Program.getDecompressed1(input);
             Assert.AreEqual(results, "(1x3)A");
         }
 
@@ -43,8 +43,40 @@ namespace Day9Tests
         public void TestCase5()
         {
             string input = "X(8x2)(3x3)ABCY";
-            string results = Day9.Program.getDecompressed(input);
+            string results = Day9.Program.getDecompressed1(input);
             Assert.AreEqual(results, "X(3x3)ABC(3x3)ABCY");
+        }
+
+        [TestMethod]
+        public void TestCaseP2_1()
+        {
+            string input = "(3x3)XYZ";
+            string results = Day9.Program.getDecompressed2(input);
+            Assert.AreEqual(results, "XYZXYZXYZ");
+        }
+
+        [TestMethod]
+        public void TestCaseP2_2()
+        {
+            string input = "X(8x2)(3x3)ABCY";
+            string results = Day9.Program.getDecompressed2(input);
+            Assert.AreEqual(results, "XABCABCABCABCABCABCY");
+        }
+
+        [TestMethod]
+        public void TestCaseP2_3()
+        {
+            string input = "(27x12)(20x12)(13x14)(7x10)(1x12)A";
+            string results = Day9.Program.getDecompressed2(input);
+            Assert.AreEqual(results.Length, 241920);
+        }
+
+        [TestMethod]
+        public void TestCaseP2_4()
+        {
+            string input = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN";
+            string results = Day9.Program.getDecompressed2(input);
+            Assert.AreEqual(results.Length, 445);
         }
     }
 }
