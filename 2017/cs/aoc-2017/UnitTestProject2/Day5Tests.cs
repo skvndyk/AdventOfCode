5055ef14cc51f45353bd9b352e5d3e5ebb6aa67a
Sarah Van Dyke <svandyke@bellarmine.edu>
2017-12-06 09:09:20 -0500

    stubbed out day5-2017

---
+++ 2017/cs/aoc-2017/UnitTestProject2/Day5Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day5;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class Day5Tests
+    {
+        [TestMethod]
+        public void Day3_P1A()
+        {
+        }
+        public void Day3_P1B()
+        {
+        }
+        public void Day3_P1C()
+        {
+        }
+        public void Day3_P1D()
+        {
+        }
+        public void Day3_P1E()
+        {
+        }
+        public void Day3_P1F()
+        {
+        }
+    }
+}
