e8a4bf5f4238f755d3068b8c3b58649e250e1f87
Sarah Van Dyke <svandyke@bellarmine.edu>
2018-03-16 16:46:00 -0400

    set up framework for 2017day8-p1

---
+++ 2017/cs/aoc-2017/Day8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Day8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "day8-2017.txt";
+            string input = System.IO.File.ReadAllText(filePath);
+            //parse each line using regex:
+            //0: register
+            //1: inc/dec (dict with +/-, generic here?)
+            //2: if
+            //3: condition
+                //register, (>, <, >=, <=, ==, !=) num
+           
+            //create Dict<string, int> for register, value
+            //translate line into "instruction"
+            //...
+        }
+    }
+}
---
+++ 2017/cs/aoc-2017/UnitTestProject2/Day8Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day8;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class Day8Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
