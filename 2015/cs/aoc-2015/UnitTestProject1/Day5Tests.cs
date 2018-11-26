c7c8578ac312bca2e8132ae75b2499313c6c7c92
Sarah Van Dyke <svandyke@bellarmine.edu>
2018-11-26 16:54:50 -0500

    added stuff for day5 tests

---
+++ 2015/cs/aoc-2015/UnitTestProject1/Day5Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day5;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Day5Tests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+          
+        }
+
+      
+    }
+}
