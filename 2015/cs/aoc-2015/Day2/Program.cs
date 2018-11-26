fba06d566cd89c0ed840a5cfca720cd443975554
Sarah Van Dyke <svandyke@bellarmine.edu>
2018-11-26 15:43:56 -0500

    finished d2p1-2015

--- 2015/cs/aoc-2015/Day2/Program.cs
+++ 2015/cs/aoc-2015/Day2/Program.cs
@@ -12,13 +12,19 @@ namespace Day2
         {
             string filePath = "day2-2015.txt";
             string input = System.IO.File.ReadAllText(filePath);
-            List<List<int>> dimensions = ParseFile(input);
-            int y = 4;
+            List<List<int>> allBoxDims = ParseFile(input);
+            Console.WriteLine(Part1(allBoxDims));
+            Console.ReadLine();
         }
 
-        public static void Part1()
+        public static int Part1(List<List<int>> allBoxDims)
         {
-
+            int sum = 0;
+            foreach (List<int> dims in allBoxDims)
+            {
+                sum += GetSurfaceArea(dims) + GetAreaOfSmallestSide(dims);
+            }
+            return sum;
         }
 
         public static void Part2()
@@ -30,6 +36,21 @@ namespace Day2
         {
             List<string> dimStrings = input.Split(Environment.NewLine.ToCharArray()).ToList();
             return dimStrings.Select(d => d.Split('x').Select(s => Int32.Parse(s)).ToList()).ToList();
-         }
+        }
+
+        public static int GetSurfaceArea(List<int> dimensions)
+        {
+            int surfaceArea = 2 * dimensions[0] * dimensions[1] + 2 * dimensions[1] * dimensions[2] +
+                   2 * dimensions[2] * dimensions[0];
+            return surfaceArea;
+        }
+
+        public static int GetAreaOfSmallestSide(List<int> dimensions)
+        {
+            List<int> newDims = new List<int>(dimensions);
+            int longestSide = newDims.Max();
+            newDims.Remove(longestSide);
+            return newDims[0] * newDims[1];
+        }
     }
 }
