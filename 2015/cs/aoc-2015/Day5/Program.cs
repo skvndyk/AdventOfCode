e5beffb1196a8a37ee6051c964596e4b73d29292
Sarah Van Dyke <svandyke@bellarmine.edu>
2018-11-27 09:04:40 -0500

    finished day5p1-2015 (just converted to c#)

--- 2015/cs/aoc-2015/Day5/Program.cs
+++ 2015/cs/aoc-2015/Day5/Program.cs
@@ -1,15 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Day5
 {
-    class Program
+    public class Program
     {
+        private static List<string> badStrings = new List<string>() { "ab", "cd", "pq", "xy" };
+        private static List<char> vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
+
         static void Main(string[] args)
         {
+            string filePath = "day5-2015.txt";
+            string input = System.IO.File.ReadAllText(filePath);
+            List<string> inputList = ParseInput(input);
+            Console.WriteLine($@"Part1 answer: {Part1(inputList)}");
+            //Console.WriteLine($@"Part2 answer: {Part2(inputList)}");
+            Console.ReadLine();
+
+        }
+
+        public static int Part1(List<string> inputList)
+        {
+            return inputList.Count(str => IsGoodString(str));
+        }
+
+        public static int Part2(List<string> inputList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<string> ParseInput(string input)
+        {
+            return input.Split(Environment.NewLine.ToCharArray()).ToList();
+        }
+
+        public static bool IsGoodString(string input) => HasThreePlusVowels(input) && HasRepetitionPairs(input) && !HasBadString(input);
+ 
+        public static bool HasThreePlusVowels(string input)
+        {
+            int numVowels = input.Count(ch => vowels.Any(v => v == ch));
+            return numVowels >= 3;
+        }
+
+        public static bool HasRepetitionPairs(string input)
+        {
+            bool hasRepetitionPair = false;
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i+1])
+                {
+                    hasRepetitionPair = true;
+                }
+            }
+            return hasRepetitionPair;
+        }
+
+        public static bool HasBadString(string input)
+        {
+            bool hasBadString = false;
+            string subString = "";
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                subString = input.Substring(i, 2);
+                if (badStrings.Any(s => s == subString))
+                {
+                    hasBadString = true;
+                    break;
+                }
+            }
+            return hasBadString;
         }
     }
 }
--- 2015/cs/aoc-2015/UnitTestProject1/Day5Tests.cs
+++ 2015/cs/aoc-2015/UnitTestProject1/Day5Tests.cs
@@ -8,11 +8,41 @@ namespace UnitTestProject1
     public class Day5Tests
     {
         [TestMethod]
-        public void Test1()
+        public void Part1_Test1()
         {
-          
+            string input = "ugknbfddgicrmopn";
+            Assert.IsTrue(Program.IsGoodString(input));
         }
 
-      
+        [TestMethod]
+        public void Part1_Test2()
+        {
+            string input = "aaa";
+            Assert.IsTrue(Program.IsGoodString(input));
+
+        }
+
+        [TestMethod]
+        public void Part1_Test3()
+        {
+            string input = "jchzalrnumimnmhp";
+            Assert.IsFalse(Program.IsGoodString(input));
+        }
+
+        [TestMethod]
+        public void Part1_Test4()
+        {
+            string input = "haegwjzuvuyypxyu";
+            Assert.IsFalse(Program.IsGoodString(input));
+        }
+
+        [TestMethod]
+        public void Part1_Test5()
+        {
+            string input = "dvszwmarrgswjxmb";
+            Assert.IsFalse(Program.IsGoodString(input));
+        }
+
+
     }
 }
