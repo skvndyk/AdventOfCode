346fee2908ee8d84ec599908951701a6d1144052
Sarah Van Dyke <svandyke@bellarmine.edu>
2018-12-06 11:53:17 -0500

    starting day3-2018p1, noooo idea

---
+++ 2018/cs/aoc-2018/aoc-2018/Day3/FabricClaim.cs
@@ -0,0 +1,11 @@
+﻿namespace Day3
+{
+    public class FabricClaim
+    {
+        public string Id { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
--- 2018/cs/aoc-2018/aoc-2018/Day3/Program.cs
+++ 2018/cs/aoc-2018/aoc-2018/Day3/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Day3
 {
-    class Program
+    public class Program
     {
+        public static readonly Regex _rgx = new Regex($@"#(\d*) @ (\d*),(\d*): (\d*)x(\d*)");
         static void Main(string[] args)
         {
+            string filePath = "day3-2018.txt";
+            List<FabricClaim> claims = ParseInput(filePath);
+            //Console.WriteLine($"Part 1: {Part1(lines)}");
+            //Console.WriteLine($"Part 2: {Part2(lines)}");
+            Console.ReadLine();
+
+        }
+
+        public static int Part1(List<int> lines)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Part2(List<int> lines)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<FabricClaim> ParseInput(string filePath)
+        {
+            return GetClaimsFromLines(ReadTextIntoLines(filePath));
+        }
+
+        public static List<string> ReadTextIntoLines(string filePath)
+        {
+            string rawInput = System.IO.File.ReadAllText(filePath);
+            return rawInput.Split('\n').ToList();
+        }
+
+        public static List<FabricClaim> GetClaimsFromLines(List<string> lines)
+        {
+            return lines.Select(ParseLineToClaim).ToList();
+        }
+        public static FabricClaim ParseLineToClaim(string line)
+        {
+            Match match = _rgx.Match(line);
+            if (!match.Success) throw new Exception($@"could not parse line with contents {line}");
+            GroupCollection groups = match.Groups;
+            return new FabricClaim()
+            {
+                Id = groups[0].Value,
+                X = int.Parse(groups[1].Value),
+                Y = int.Parse(groups[2].Value),
+                Height = int.Parse(groups[3].Value),
+                Width = int.Parse(groups[4].Value),
+            };
         }
     }
 }
