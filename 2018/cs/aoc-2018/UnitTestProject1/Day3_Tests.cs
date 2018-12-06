7e259c92bf08ae04744da233bc442de62071581d
Sarah Van Dyke <svandyke@bellarmine.edu>
2018-12-06 11:33:55 -0500

    added framework for Day3

---
+++ 2018/cs/aoc-2018/UnitTestProject1/Day3_Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Day3_Tests
+    {
+
+        [TestMethod]
+        public void Test1()
+        {
+            
+        }   
+    }
+}
