62c1c27fbaa5b30c85bbc12a72b9077de9eaee03
Sarah Van Dyke <svandyke@bellarmine.edu>
2018-12-06 11:32:08 -0500

    not sure why this works now, but day2-2018p2 is done

--- 2018/cs/aoc-2018/Day2/Program.cs
+++ 2018/cs/aoc-2018/Day2/Program.cs
@@ -51,37 +51,35 @@ namespace Day2
         public static string Part2(List<string> boxIds)
         {
             int idCharLength = boxIds[0].Length;
-         
-            for (int i = 0; i < boxIds.Count - 2; i++)
-            {
-                for (int j = i + 1; j < boxIds.Count - 1; j++)
-                {
-                    Dictionary<char, int> matchCharIdxDict = new Dictionary<char, int>();
 
-                    for (int k = 0; k < idCharLength; k++)
+            for (int i = 0; i < boxIds.Count - 1; i++)
+            {
+                for (int j = i + 1; j < boxIds.Count; j++)
+                {
+                    Dictionary<int, char> matchCharIdxDict = new Dictionary<int, char>();
+
+                    for (int k = 0; k < idCharLength && k <= matchCharIdxDict.Count + 1; k++)
                     {
                         if (boxIds[i][k] == boxIds[j][k])
                         {
-                            matchCharIdxDict[boxIds[i][k]] = k;
-                        }
-                        if (k > matchCharIdxDict.Count + 1)
-                        {
-                            break;
+                            matchCharIdxDict[k] = boxIds[i][k];
                         }
                     }
+                
                     if (matchCharIdxDict.Count == idCharLength - 1)
                     {
-                        return string.Concat(matchCharIdxDict.OrderBy(x => x.Value).Select(p => p.Key));
+                        return string.Concat(matchCharIdxDict.OrderBy(x => x.Key).Select(p => p.Value));
                     }
                 }
             }
+
             return null;
         }
 
-        public static List<string> ReadTextIntoLines(string filePath)
-        {
-            string rawInput = System.IO.File.ReadAllText(filePath);
-            return rawInput.Split('\n').ToList();
-        }
+    public static List<string> ReadTextIntoLines(string filePath)
+    {
+        string rawInput = System.IO.File.ReadAllText(filePath);
+        return rawInput.Split('\n').ToList();
     }
+}
 }
