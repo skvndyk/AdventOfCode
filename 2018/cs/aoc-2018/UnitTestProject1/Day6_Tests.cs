0e5dc7ff463ab9704c8e921e7b7a2b47383153b1
Sarah Van Dyke <svandyke@bellarmine.edu>
2018-12-12 09:17:51 -0500

    adding stuff for day6-2018

---
+++ 2018/cs/aoc-2018/UnitTestProject1/Day6_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day5;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Day6_Tests
+    {
+
+        [TestMethod]
+        public void Part1_T1()
+        {
+           
+
+        }
+
+       
+    }
+}
---
+++ 2018/cs/aoc-2018/aoc-2018/Day6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "day6-2018.txt";
+            List<int> lines = ReadTextIntoLines(filePath);
+            Console.WriteLine($"Part 1: {Part1(lines)}");
+            Console.WriteLine($"Part 2: {Part2(lines)}");
+            Console.ReadLine();
+
+        }
+
+        public static int Part1(List<int> lines)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Part2(List<int> lines)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<int> ReadTextIntoLines(string filePath)
+        {
+            string rawInput = System.IO.File.ReadAllText(filePath);
+            return rawInput.Replace("+", string.Empty).Split('\n').Select(int.Parse).ToList();
+        }
+    }
+}
