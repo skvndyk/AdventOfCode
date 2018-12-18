00be231c036ad48b3c272929a28f5b54addfa2ec
Sarah Van Dyke <svandyke@bellarmine.edu>
2018-12-18 16:43:42 -0500

    cleaning up day7-2018p2 stuff

--- 2018/cs/aoc-2018/Day7/Models/Step.cs
+++ 2018/cs/aoc-2018/Day7/Models/Step.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day7
+namespace Day7.Models
 {
     public class Step
     {
---
+++ 2018/cs/aoc-2018/Day7/Models/Worker.cs
@@ -0,0 +1,8 @@
+﻿namespace Day7.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string CurrentStep { get; set; }
+    }
+}
