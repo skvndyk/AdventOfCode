607f6a658391661fbd9c296a3bd95fb8fc0a793b
Sarah Van Dyke <sarah.vandyke@waystar.com>
2019-08-29 13:58:25 -0400

    added framework for day9-2018

---
+++ 2018/cs/aoc-2018/Day10/Models/Grid.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10.Models
+{
+    public class Grid
+    {
+        public List<Point> Points { get; set; }
+
+        public int MaxX => Points.Max(p => p.InitialPosition.X);
+        public int MinX => Points.Min(p => p.InitialPosition.X);
+
+        public int MaxY => Points.Max(p => p.InitialPosition.Y);
+        public int MinY => Points.Min(p => p.InitialPosition.X);
+
+        public void ApplyVelocities()
+        {
+            foreach (var point in Points)
+            {
+                point.ApplyVelocity();
+            }
+        }
+
+        public void PrintGrid()
+        {
+            List<string> toPrint = new List<string>();
+            for (int i = MinX; i <= MaxX; i++)
+            {
+                string lineToPrint = "";
+
+                for (int j = MinY; j < MaxY; j++)
+                {
+                    lineToPrint += Points.Where(p => p.CurrentPosition.X == i && p.CurrentPosition.X == j)?
+                        .First() == null ? "." : "#";
+                   
+                }
+
+                toPrint.Add(lineToPrint);
+            }
+
+            Console.WriteLine(string.Join("\n", toPrint));
+        }
+    }
+}
---
+++ 2018/cs/aoc-2018/Day10/Models/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10.Models
+{
+    public class Point
+    {
+        public Position InitialPosition { get; set; }
+        public Position CurrentPosition { get; set; }
+        public Position Velocity { get; set; }
+
+        public void ApplyVelocity()
+        {
+            CurrentPosition.X += Velocity.X;
+            //for some reason their coord system has the Y-axis flipped
+            CurrentPosition.Y += -Velocity.Y;
+        }
+    }
+
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
