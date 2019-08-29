b32c2cc81950be42688a5c1be2fbb4da8a4a3d30
Sarah Van Dyke <sarah.vandyke@waystar.com>
2019-08-29 14:31:49 -0400

    parsing file into points

--- 2018/cs/aoc-2018/Day10/Program.cs
+++ 2018/cs/aoc-2018/Day10/Program.cs
@@ -2,14 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Day10.Models;
 
 namespace Day10
 {
     class Program
     {
-        static void Main(string[] args)
+        public static readonly Regex _gridRgx = 
+            new Regex(@"position=<(?'posX'-?\d*),(?'posY'-?\d*)>velocity=<(?'velX'-?\d*),?(?'velY'-?\d*)>");
+        public static void Main(string[] args)
         {
+            string fileName = "day10-2018-example.txt";
+            Grid grid = ParseInputFile(fileName);
+        }
+
+        public static Grid ParseInputFile(string fileName)
+        {
+            List<Point> points = new List<Point>();
+
+            List<string> lines = File.ReadAllLines(fileName).ToList();
+            foreach (var line in lines)
+            {
+                string trimmedLine = line.Replace(" ", "");
+                MatchCollection matches = _gridRgx.Matches(trimmedLine);
+                foreach (Match match in matches)
+                {
+                    GroupCollection groups = match.Groups;
+
+                    Point point = new Point()
+                    {
+                        InitialPosition = new Position()
+                        {
+                            X = Convert.ToInt32(groups["posX"].Value),
+                            Y = Convert.ToInt32(groups["posY"].Value)
+                        },
+
+                        Velocity = new Position()
+                        {
+                            X = Convert.ToInt32(groups["velX"].Value),
+                            Y = Convert.ToInt32(groups["velY"].Value)
+                        },
+                    };
+
+                    point.CurrentPosition = point.InitialPosition;
+                    points.Add(point);
+                }
+            }
+
+            return new Grid()
+            {
+                Points = points
+            };
         }
     }
 }
