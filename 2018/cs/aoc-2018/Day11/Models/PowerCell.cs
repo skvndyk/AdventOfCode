7ec44a2ce7fecfa17405fc31ddf0b9d779ab3397
Sarah Van Dyke <sarah.vandyke@waystar.com>
2019-08-30 13:14:10 -0400

    data structures in place, functions for calculating powercell properties

---
+++ 2018/cs/aoc-2018/Day11/Models/PowerCell.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11.Models
+{
+    public class PowerCell
+    {
+        public Position TopLeftCorner { get; set; }
+        public List<Position> PositionsCovered { get; set; }
+
+        public void SetPositionsCovered()
+        {
+            if (TopLeftCorner != null)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    for (int y = 0; y < 3; y++)
+                    {
+                        PositionsCovered.Add(new Position()
+                        {
+                            X = TopLeftCorner.X + x,
+                            Y = TopLeftCorner.Y + y
+                        });
+                    }
+                }
+            }
+        }
+
+        public void SetTopLeftCorner()
+        {
+            if (PositionsCovered != null)
+            {
+                TopLeftCorner = PositionsCovered.Where(p => p.X == PositionsCovered.Min(p1 => p1.X) && 
+                p.Y == PositionsCovered.Min(p2 => p2.Y)).First();
+            }
+        }
+    }
+}
