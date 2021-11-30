2d850fd2f5e7ad837b551fabed1a86688104d98f
Sarah Van Dyke <Sarah.VanDyke@ceridian.com>
2021-11-30 13:50:04 -0500

    Getting ready for AOC 2021 Day1!

---
+++ 2021/cs/aoc-2021/aoc-2021/Day1_2021.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aoc_2021
+{
+    class Day1_2021
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string filePath = "day1-2021.txt";
+            List<string> inputStrings = Common.Utilities.ReadFileToStrings(filePath);
+        }
+    }
+}
