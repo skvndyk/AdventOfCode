7a42db384abbce900599f591dd258c722a5954c7
Sarah Van Dyke <Sarah.VanDyke@ceridian.com>
2021-11-30 13:45:15 -0500

    Creating Common library

---
+++ Common/Common/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Utilities
+    {
+        public static List<string> ReadFileToStrings(string filePath)
+        {
+            var stringArr = File.ReadAllLines(filePath);
+            return stringArr.ToList();
+        }
+
+        //public static List<GroupCollection> RegExSplitter(string pattern, string text)
+        //{
+        //    var rx = new Regex(pattern);
+        //    MatchCollection matches = rx.Matches(text);
+        //    return matches;
+        //}
+    }
+}
