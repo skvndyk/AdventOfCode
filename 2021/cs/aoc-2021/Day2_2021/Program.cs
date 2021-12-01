2dce80f3dc246697e448ec85d7037e2d49c2354c
Sarah Van Dyke <Sarah.VanDyke@ceridian.com>
2021-12-01 11:20:51 -0500

    Setting up Day2-2021

---
+++ 2021/cs/aoc-2021/Day2_2021/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day2_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string filePath = "day2-2021.txt";
+            string exFilePath = "day2-ex-2021.txt";
+            List<string> inputStrings = Common.Utilities.ReadFileToStrings(filePath);
+
+            //Console.WriteLine($@"Part 1 Answer:{ParseInputLinesPart1(inputStrings)}");
+            //Console.WriteLine($@"Part 2 Answer:{ParseInputLinesPart2(inputStrings)}");
+            Console.ReadLine();
+        }
+
+        private static void ParseInputLinesPart1(List<string> inputStrings)
+        {
+           
+        }
+
+        private static void ParseInputLinesPart2(List<string> inputStrings)
+        {
+
+        }
+    }
+}
