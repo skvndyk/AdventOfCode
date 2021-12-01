1fd3b73fea452eda78e3ea2390bf9b225227d6f3
Sarah Van Dyke <Sarah.VanDyke@ceridian.com>
2021-12-01 09:48:17 -0500

    Completed Day1-2021-P1

--- 2021/cs/aoc-2021/aoc-2021/Day1_2021.cs
+++ 2021/cs/aoc-2021/aoc-2021/Day1_2021.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 
 namespace aoc_2021
 {
-    class Day1_2021
+    public class Day1_2021
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             string filePath = "day1-2021.txt";
             List<string> inputStrings = Common.Utilities.ReadFileToStrings(filePath);
+
+            //count the number of times a depth measurement increases from the previous measurement
+            Console.WriteLine($@"Part 1 Answer:{ParseInputLinesPart1(inputStrings)}");
+            Console.ReadLine();
+        }
+
+        private static int ParseInputLinesPart1(List<string> inputStrings)
+        {
+            var depths = GetIntsFromInputStrings(inputStrings);
+            var increaseCtr = 0;
+            var depthCtr = 0;
+            var prevDepth = depths[0];
+            foreach (var depth in depths)
+            {
+                if (depth > prevDepth)
+                {
+                    increaseCtr++;
+                }
+
+                prevDepth = depths[depthCtr];
+                depthCtr++;
+ 
+            }
+            return increaseCtr;
+        }
+
+        private static List<int> GetIntsFromInputStrings(List<string> inputStrings)
+        {
+            var depths = new List<int>();
+            foreach (var str in inputStrings)
+            {
+                depths.Add(int.Parse(str));
+            }
+
+            return depths;
         }
     }
 }
