26c3ef41e1c8155f4cbafb3a6c7a51c2486e52c3
Sarah Van Dyke <Sarah.VanDyke@ceridian.com>
2021-11-30 20:20:33 -0500

    Preparing Day3-2020-p1

--- 2020/cs/aoc-2020/Day2_2020/Program.cs
+++ 2020/cs/aoc-2020/Day2_2020/Program.cs
@@ -21,22 +21,41 @@ namespace Day2_2020
         {
             var exFilePath = "day2-ex-2020.txt";
             var filePath = "day2-2020.txt";
-            Console.WriteLine($@"Part 1: {ParseInputFile1(filePath)} matching passwords");
+            var inputLines = Common.Utilities.ReadFileToStrings(filePath);
+            Console.WriteLine($@"Part 1: {ParseInputFile1(inputLines)} matching passwords");
+            Console.WriteLine($@"Part 2: {ParseInputFile2(inputLines)} matching passwords");
             Console.ReadLine();
         }
 
-        public static int ParseInputFile1(string filePath)
+        public static int ParseInputFile1(List<string> inputLines)
         {
             int numMatchingPasswords = 0;
-            var exLines = Common.Utilities.ReadFileToStrings(filePath);
-            foreach (var line in exLines)
+            foreach (var line in inputLines)
             {
                 MatchCollection matches = _patternRgx.Matches(line);
                 foreach (Match match in matches)
                 {
-                    var passwordRule = new PasswordRule(match);
-                    var numRepetitions = passwordRule.Password.Count(l => l == passwordRule.RequiredLetter.ToCharArray()[0]);
+                    var passwordRule = new PasswordRule(match, PuzzlePart.One);
+                    var numRepetitions = passwordRule.Password.Count(l => l == passwordRule.RequiredLetter);
                     if (numRepetitions >= passwordRule.MinRepetition && numRepetitions <= passwordRule.MaxRepetition)
+                    {
+                        numMatchingPasswords++;
+                    }
+                }
+            }
+            return numMatchingPasswords;
+        }
+
+        public static int ParseInputFile2(List<string> inputLines)
+        {
+            int numMatchingPasswords = 0;
+            foreach (var line in inputLines)
+            {
+                MatchCollection matches = _patternRgx.Matches(line);
+                foreach (Match match in matches)
+                {
+                    var passwordRule = new PasswordRule(match, PuzzlePart.Two);
+                    if (passwordRule.Password[passwordRule.Position1 - 1] == passwordRule.RequiredLetter ^ passwordRule.Password[passwordRule.Position2 - 1] == passwordRule.RequiredLetter)
                     {
                         numMatchingPasswords++;
                     }
@@ -49,16 +68,29 @@ namespace Day2_2020
         {
             public int MinRepetition { get; set; }
             public int MaxRepetition { get; set; }
-            public string RequiredLetter { get; set; }
+            public int Position1 {get; set;}
+            public int Position2 { get; set; }
+            public char RequiredLetter { get; set; }
             public string Password { get; set; }
 
-            public PasswordRule(Match match)
+            public PasswordRule(Match match, PuzzlePart puzzlePart)
             {
-                MinRepetition = int.Parse(match.Groups["minRepetition"].Value);
-                MaxRepetition = int.Parse(match.Groups["maxRepetition"].Value);
-                RequiredLetter = match.Groups["reqLetter"].Value;
+                switch (puzzlePart)
+                {
+                    case PuzzlePart.One:
+                        MinRepetition = int.Parse(match.Groups["minRepetition"].Value);
+                        MaxRepetition = int.Parse(match.Groups["maxRepetition"].Value);
+                        break;
+                    case PuzzlePart.Two:
+                        Position1 = int.Parse(match.Groups["minRepetition"].Value);
+                        Position2 = int.Parse(match.Groups["maxRepetition"].Value);
+                        break;
+                }
+                RequiredLetter = match.Groups["reqLetter"].Value.ToCharArray()[0];
                 Password = match.Groups["password"].Value;
             }
         }
+
+        public enum PuzzlePart { One, Two}
     }
 }
---
+++ 2020/cs/aoc-2020/Day3_2020/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day3_2020
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var exFilePath = "day3-ex-2020.txt";
+            var filePath = "day3-2020.txt";
+            int rise = 3;
+            int run = 1;
+            var inputLines = Common.Utilities.ReadFileToStrings(exFilePath);
+            ParseInputFile1(inputLines, rise, run);
+            //Console.WriteLine($@"Part 2: {ParseInputFile2(inputLines)}");
+            Console.ReadLine();
+        }
+
+        private static void ParseInputFile1(List<string> inputLines, int rise, int run)
+        {
+            var grid = new Grid();
+            Setup(inputLines, grid);
+            
+        }
+
+        public static void Setup(List<string> inputLines, Grid grid)
+        {
+            var rowCtr = 0;
+
+            Console.WriteLine("Initial Config");
+
+            foreach (var line in inputLines)
+            {
+                var row = new Row { RowNum = rowCtr };
+                var colCtr = 0;
+
+                foreach (var symbol in line)
+                {
+                    row.Squares.Add(new Square()
+                    {
+                        XCoord = colCtr,
+                        YCoord = rowCtr,
+                        Contents = symbol
+                    });
+                    colCtr++;
+                }
+
+                Console.WriteLine(row.ToString());
+                grid.Rows.Add(row);
+                rowCtr++;
+            }
+        }
+
+
+        private static void ApplySlope(Grid grid)
+        {
+
+        }
+
+        public class Grid
+        {
+            public List<Row> Rows { get; set; } = new List<Row>();
+            public int Rise { get; set; }
+            public int Run { get; set; }
+
+            public Grid(int rise, int run)
+            {
+                Rise = rise;
+                Run = run;
+            }
+        }
+
+        public class Row
+        {
+            public int RowNum { get; set; }
+            public List<Square> Squares { get; set; } = new List<Square>();
+            public override string ToString() => new string(Squares.Select(s => s.Contents).ToArray());
+        }
+
+        public class Square
+        {
+            public int XCoord { get; set; }
+            public int YCoord { get; set; }
+            public char Contents { get; set; }
+            public Annotation Annotation = Annotation.None;
+        }
+
+        public class Slope
+        {
+            int Rise { get; set; }
+            int Run { get; set; }
+        }
+        public enum Annotation { None, X, O }
+    }
+}
