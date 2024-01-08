518f1d4a5af6c6dc371f12e48736189923b65121
Sarah Van Dyke <Sarah.VanDyke@ceridian.com>
2024-01-08 15:56:19 -0500

    2023-Day4-P2 complete

--- AOC/AoC2023/Day4/Day4_Program.cs
+++ AOC/AoC2023/Day4/Day4_Program.cs
@@ -17,10 +17,10 @@ namespace AoC2023.Day1
             var exInputStrings2 = Common.Utilities.ReadFileToStrings(exFilePath2);
             var inputStrings = Common.Utilities.ReadFileToStrings(filePath);
 
-            Console.WriteLine($"Part 1 example answer: {Part1(exInputStrings1)}");
+            //Console.WriteLine($"Part 1 example answer: {Part1(exInputStrings1)}");
             Console.WriteLine($"Part 2 example answer: {Part2(exInputStrings2)}");
 
-            Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
+            //Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
             Console.WriteLine($"Part 2 answer: {Part2(inputStrings)}");
         }
 
@@ -32,55 +32,90 @@ namespace AoC2023.Day1
 
         private static int Part2(List<string> inputStrings)
         {
-            return 0;
+            var game = InputStringsToGame(inputStrings);
+            while (game.UnprocessedCardQueue.Count > 0)
+            {
+                var currCard = game.UnprocessedCardQueue.Dequeue();
+                for (int i = 1; i <= currCard.WinningPlayerNumbersCount; i++)
+                {
+                    var newId = currCard.Id + i;
+                    var cardToCopy = game.FindCardById(newId);
+                    game.UnprocessedCardQueue.Enqueue(
+                        new Card (
+                            newId,
+                            cardToCopy.WinningNumbers,
+                            cardToCopy.PlayerNumbers
+                        )
+                    );
+                }
+                game.ProcessedCardQueue.Enqueue(currCard);
+            }
+            return game.ProcessedCardQueue.Count;
         }
 
         private static Game InputStringsToGame(List<string> inputStrings)
         {
             var game = new Game();
 
-            string pattern = $@"(Card *\d+:)(.*)";
+            string pattern = $@"(Card *(\d+):)(.*)";
             Regex regex = new Regex(pattern);
 
             foreach (var str in inputStrings)
             {
                 var matches = regex.Matches(str);
-                var cardContents = matches[0].Groups[2].Value;
+                var cardContents = matches[0].Groups[3].Value;
+                var cardId = int.Parse(matches[0].Groups[2].Value);
 
                 var cardList = cardContents.Split('|');
                 var winningNumbers = cardList[0].Split(' ').Where(x => x != string.Empty).ToList();
                 var playerNumbers = cardList[1].Split(' ').Where(x => x != string.Empty).ToList();
 
-                game.Cards.Add(new Card(winningNumbers, playerNumbers));
+                game.Cards.Add(new Card(cardId, winningNumbers, playerNumbers));
 
             }
+            //initialize game queue
+            game.Cards.ForEach(c => game.UnprocessedCardQueue.Enqueue(c));
             return game;
         }
 
         public class Game
         {
             public List<Card> Cards { get; set; } = new List<Card>();
+            public Queue<Card> UnprocessedCardQueue { get; set; } = new Queue<Card>();
+            public Queue<Card> ProcessedCardQueue { get; set;} = new Queue<Card>();
+            public Card FindCardById(int id) => Cards.FirstOrDefault(c => c.Id == id);
         }
         public class Card
         {
-            List<int> WinningNumbers { get; set; } = new List<int>();
-            List<int> PlayerNumbers { get; set; } = new List<int>();
+            public int Id { get; set; }
+            public List<int> WinningNumbers { get; set; } = new List<int>();
+            public List<int> PlayerNumbers { get; set; } = new List<int>();
 
             public List<int> WinningPlayerNumbers => WinningNumbers.Intersect(PlayerNumbers).ToList();
+            public int WinningPlayerNumbersCount => WinningPlayerNumbers.Count;
 
-            public Card(List<int> winningNumbers, List<int> playerNumbers)
+            public Card(int id, List<int> winningNumbers, List<int> playerNumbers)
             {
+                Id = id;
                 WinningNumbers = winningNumbers;
                 PlayerNumbers = playerNumbers;
             }
 
-            public Card(List<string> winningNumbers, List<string> playerNumbers)
+            public Card(int id, List<string> winningNumbers, List<string> playerNumbers)
             {
+                Id = id;
                 WinningNumbers = winningNumbers.Select(int.Parse).ToList();
                 PlayerNumbers = playerNumbers.Select(int.Parse).ToList();
             }
 
             public int PointValue => (int)Math.Pow(2, WinningPlayerNumbers.Count - 1); 
+
+            public Card CopyCard()
+            {
+                return new Card(Id, WinningNumbers, PlayerNumbers);
+            }
         }
+
+
     }
 }
