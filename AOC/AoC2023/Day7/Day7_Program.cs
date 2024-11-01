c2a523ce8be731336ba19b1d29a2a10743ca4515
Sarah <sarah.vandyke@dayforce.com>
2024-11-01 12:36:15 -0400

    day 7 2023 example working, p1 not

--- AOC/AoC2023/Day7/Day7_Program.cs
+++ AOC/AoC2023/Day7/Day7_Program.cs
@@ -15,20 +15,27 @@ namespace AoC2023.Day1
             string exFilePath2 = $@"Day7\exInputFileDay7-2023_P2.txt";
 
             var exInputStrings1 = Common.Utilities.ReadFileToStrings(exFilePath1);
-            //var exInputStrings2 = Common.Utilities.ReadFileToStrings(exFilePath2);
-            //var inputStrings = Common.Utilities.ReadFileToStrings(filePath);
+            var exInputStrings2 = Common.Utilities.ReadFileToStrings(exFilePath2);
+            var inputStrings = Common.Utilities.ReadFileToStrings(filePath);
 
-            Console.WriteLine($"Part 1 example answer: {Part1(exInputStrings1)}");
+            //Console.WriteLine($"Part 1 example answer: {Part1(exInputStrings1)}");
             //Console.WriteLine($"Part 2 example answer: {Part2(exInputStrings2)}");
 
-            //Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
+            Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
             //Console.WriteLine($"Part 2 answer: {Part2(inputStrings)}");
         }
 
         private static int Part1(List<string> inputStrings)
         {
             var hands = ConvertInputToDataStructures(inputStrings);
-            return 0;
+            var rankedHands = RankHands(hands);
+            rankedHands.Reverse();
+            var sum = 0;
+            for (int i = 0; i < rankedHands.Count; i++)
+            {
+                sum += rankedHands[i].Bid * (i + 1);
+            }
+            return sum;
         }
 
         private static int Part2(List<string> inputStrings)
@@ -36,7 +43,65 @@ namespace AoC2023.Day1
             return 0;
         }
 
-       
+        private static List<Hand> RankHands(List<Hand> hands)
+        {
+            var rankedHands = new List<Hand>();
+            var dict = new Dictionary<HandType, List<Hand>>();
+            foreach (var hand in hands)
+            {
+                var newKeyAdded = dict.TryAdd(hand.HandType, new List<Hand>() { hand });
+                if (!newKeyAdded)
+                {
+                    dict[hand.HandType].Add(hand);
+                }
+            }
+
+            var orderedDict = dict.OrderByDescending(kvp => kvp.Key).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            var orderedHandsByType = new List<Hand>();
+            var maxCard = new Card();
+            foreach (var rec in orderedDict.Keys)
+            {
+                orderedHandsByType = new List<Hand>(orderedDict[rec]);
+                //if there's no tie, just add to list
+                if (orderedDict[rec].Count == 1)
+                {
+                    rankedHands.Add(orderedDict[rec][0]);
+                }
+                else
+                {
+                    //if there's a tie sort by highest card
+                    //get max for each index
+                    for (int i = 0; i < 5 && orderedHandsByType.Count > 1; i++)
+                    {
+                        maxCard = new Card();
+                        var maxVal = 0;
+                        for (int j = 0; j < orderedHandsByType.Count; j++)
+                        {
+                            if (orderedHandsByType[j].Cards[i].CardValue == maxVal)
+                            {
+                                maxCard = new Card();
+                                maxVal = 0;
+                                break;
+                            }
+                            else if (orderedHandsByType[j].Cards[i].CardValue > maxVal)
+                            {
+                                maxVal = orderedHandsByType[j].Cards[i].CardValue;
+                                maxCard = orderedHandsByType[j].Cards[i];
+                            }
+                        }
+                        if (maxVal != 0)
+                        {
+                            var toAddRemove = orderedHandsByType.First(h => h.Cards.Contains(maxCard));
+                            rankedHands.Add(toAddRemove);
+                            orderedHandsByType.Remove(toAddRemove);
+                        }
+                    }
+                    rankedHands.Add(orderedHandsByType[0]);
+                }
+            }
+
+            return rankedHands;
+        }
         private static List<Hand> ConvertInputToDataStructures(List<string> inputStrings)
         {
             var hands = new List<Hand>();
@@ -46,7 +111,7 @@ namespace AoC2023.Day1
                 for (int i = 1; i < cardsString.Length+1; i++) {
                     hand.Cards.Add(new Card(cardsString[i-1], i));
                 }
-                hand.Bid = int.Parse(inputString[7..]);
+                hand.Bid = int.Parse(inputString[6..]);
                 hand.CalculateHandType();
                 hands.Add(hand);
 
@@ -67,12 +132,50 @@ namespace AoC2023.Day1
                 Bid = 0;
             }
 
+            public override string ToString()
+            {
+                return string.Join("-", Cards.Select(c => c.CardValue.ToString()));
+            }
+
             public void CalculateHandType()
             {
                 var cardCounts = Cards.GroupBy(c => c.CardValue).Select(g => new {CardValue = g.Key, Count = g.Count()}).ToList();
                 var ccDict = cardCounts.ToDictionary(c => c.CardValue, c => c.Count);
+                if (ccDict.Count == 1)
+                {
+                    HandType = HandType.FiveOfAKind;
+                }
+                else if (ccDict.Count == 2)
+                {
+                    if (ccDict.ContainsValue(4))
+                    {
+                        HandType = HandType.FourOfAKind;
+                    }
+                    else
+                    {
+                        HandType = HandType.FullHouse;
+                    }
+                }
+                else if (ccDict.Count == 3)
+                {
+                    if (ccDict.ContainsValue(3))
+                    {
+                        HandType = HandType.ThreeOfAKind;
+                    }
+                    else
+                    {
+                        HandType = HandType.TwoPair;
+                    }
+                }
+                else if (ccDict.Count == 4)
+                {
+                    HandType = HandType.OnePair;
+                }
+                else
+                {
+                    HandType = HandType.HighCard;
+                }
             }
-        
         }
 
         public class Card
@@ -98,6 +201,11 @@ namespace AoC2023.Day1
                 };
                 CardValue = cardValue;
                 Index = index;
+            }
+
+            public override string ToString()
+            {
+                return $"Card: {CardValue} at index {Index}";
             }
         }
 
