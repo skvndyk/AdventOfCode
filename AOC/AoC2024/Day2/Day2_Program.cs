366119c410d13c4069d48de9d85c53184a0ea4c8
Sarah <sarah.vandyke@dayforce.com>
2025-01-08 13:28:51 -0500

    getting close to day 2 p2 2024

--- AOC/AoC2024/Day2/Day2_Program.cs
+++ AOC/AoC2024/Day2/Day2_Program.cs
@@ -20,16 +20,14 @@ namespace AoC2024.Day1
             //Console.WriteLine($"Example Part 1 answer: {Part1(exInputStrings1)}");
             //Console.WriteLine($"Example Part 2 answer: {Part2(exInputStrings2)}");
 
-            Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
-            //Console.WriteLine($"Part 2 answer: {Part2(inputStrings)}");
+            //Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
+            Console.WriteLine($"Part 2 answer: {Part2(inputStrings)}");
         }
 
         private static int Part1(List<string> inputStrings)
         {
             var blah = ParseInputStrings(inputStrings);
-            var safeRecords = new List<List<int>>();
             var numSafeRecords = 0;
-            var numRecordsProcessed = 0;
             foreach (var record in blah)
             {
 
@@ -47,7 +45,7 @@ namespace AoC2024.Day1
                 }
                 for (int i = 1; i < record.Count - 1; i++)
                 {
-                    if ((record[i] > record[i + 1] && isAscending) || (record[i] < record[i + 1] && !isAscending) || (record[i] == record[i+1]))
+                    if ((record[i] > record[i + 1] && isAscending) || (record[i] < record[i + 1] && !isAscending) || (record[i] == record[i + 1]))
                     {
                         //Console.WriteLine("either not ascending or not descending or they're equal");
                         break;
@@ -64,19 +62,107 @@ namespace AoC2024.Day1
                         else if (i == record.Count - 2)
                         {
                             numSafeRecords++;
-                            safeRecords.Add(record);
                             break;
                         }
                     }
                 }
-                numRecordsProcessed++;
             }
             return numSafeRecords;
         }
 
         private static int Part2(List<string> inputStrings)
         {
-            return 0;
+            var reportList = ParseInputStrings(inputStrings);
+            var numSafeReports = 0;
+            var numReportsProcessed = 0;
+
+            foreach (var report in reportList)
+            {
+                var badLevels = AnalyzeRecord(report);
+
+                Console.WriteLine($"Report {numReportsProcessed + 1} ({string.Join(" ", report)}) originally has {badLevels.Count}");
+                if (badLevels.Count > 0)
+                {
+                    foreach (var key in badLevels.Keys)
+                    {
+                        Console.WriteLine($"Level {key + 1}: {badLevels[key]}");
+                    }
+
+                    
+                    Console.WriteLine("Attempting to fix report");
+                    var reportFixable = false;
+                    //add first element manually, otherwise we always assume it's the 2nd element's fault
+                    badLevels[0] = report[0];
+                    foreach (var candidate in badLevels.Keys)
+                    {
+                        var fixedReport = new List<int>(report);
+                        fixedReport.RemoveAt(candidate);
+                        Console.WriteLine($"Tried to fix report by removing element at level {candidate + 1} ({badLevels[candidate]})");
+                        Console.WriteLine($"Fixed report: {string.Join(" ", fixedReport)}");
+                        if (AnalyzeRecord(fixedReport).Count == 0)
+                        {
+                            Console.WriteLine("Report fixed!\n");
+                            reportFixable = true;
+                            numSafeReports++;
+                            break;
+                        }
+                    }
+                    if (!reportFixable)
+                    {
+                        Console.WriteLine("Could not fix report\n");
+                    }
+                
+                }
+                else
+                {
+                    Console.WriteLine("Report is safe\n");
+                    numSafeReports++;
+                }
+                numReportsProcessed++;
+            }
+
+
+            return numSafeReports;
+        }
+
+        private static Dictionary<int, int> AnalyzeRecord(List<int> report)
+        {
+            var badLevels = new Dictionary<int, int>();
+            var isAscending = false;
+            if (report[0] == report[1] || (Math.Abs(report[0] - report[1]) > 3))
+            {
+                //Console.WriteLine("either first 2 are equal or difference is greater than 3");
+                badLevels[1] = report[1];
+            }
+
+            if (report[0] < report[1])
+            {
+                isAscending = true;
+            }
+            for (int i = 1; i < report.Count - 1; i++)
+            {
+                if ((report[i] > report[i + 1] && isAscending) || (report[i] < report[i + 1] && !isAscending) || (report[i] == report[i + 1]))
+                {
+                    //Console.WriteLine("either not ascending or not descending or they're equal");
+                    badLevels[i + 1] = report[i + 1];
+                }
+
+                else
+                {
+                    if (Math.Abs(report[i] - report[i + 1]) > 3)
+                    {
+                        //Console.WriteLine("difference greater than 3");
+                        badLevels[i + 1] = report[i + 1];
+                    }
+
+                    else if (i == report.Count - 2)
+                    {
+                        break;
+                    }
+                }
+            }
+            return badLevels;
+
         }
 
         private static List<List<int>> ParseInputStrings(List<string> strings)
