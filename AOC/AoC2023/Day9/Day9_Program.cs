6cf788ebe2116c888cefb19cabf56c177a4abfcd
Sarah <sarah.vandyke@dayforce.com>
2025-01-27 16:24:45 -0500

    day 9 p2 2023

--- AOC/AoC2023/Day9/Day9_Program.cs
+++ AOC/AoC2023/Day9/Day9_Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -17,17 +18,17 @@ namespace AoC2023.Day9
             //Console.WriteLine($"Part 1 example answer: {Part1(exInputStrings)}");
             //Console.WriteLine($"Part 2 example answer: {Part2(exInputStrings)}");
 
-            Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
-            //Console.WriteLine($"Part 2 answer: {Part2(inputStrings)}");
+            //Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
+            Console.WriteLine($"Part 2 answer: {Part2(inputStrings)}");
         }
 
         private static int Part1(List<string> inputStrings)
         {
-            var bloop = ParseInputStrings(inputStrings);
+            var parsedStrings = ParseInputStrings(inputStrings);
             var runningSum = 0;
-            foreach (var item in bloop)
+            foreach (var parsedString in parsedStrings)
             {
-                runningSum += ComputeNextValue(item);
+                runningSum += ComputeNextValue(parsedString);
             }
             return runningSum;
 
@@ -35,18 +36,63 @@ namespace AoC2023.Day9
 
         private static int Part2(List<string> inputStrings)
         {
-            return 0;
+            var parsedStrings = ParseInputStrings(inputStrings);
+            var runningSum = 0;
+            foreach (var parsedString in parsedStrings)
+            {
+                runningSum += ComputeNextValueP2(parsedString);
+            }
+            return runningSum;
         }
 
         private static int ComputeNextValue(List<int> values)
         {
-            var sequences = new List<List<int>>();
-            sequences.Add(values);
+            var sequences = new List<List<int>>
+            {
+                values
+            };
             var newSequence = new List<int>();
             var currSequence = values;
-            while (!newSequence.Any() || !newSequence.All(s => s == newSequence[0]))
+            while (newSequence.Count == 0 || !newSequence.All(s => s == 0))
             {
-                newSequence = new List<int>();
+                newSequence = [];
+                for (int i = 0; i < currSequence.Count - 1; i++)
+                {
+                    newSequence.Add(currSequence[i + 1] - currSequence[i]);
+                }
+
+                sequences.Add(newSequence);
+                currSequence = newSequence;
+            }
+           
+            for (int i = 0; i < sequences.Count; i++)
+            {
+                var numLeadingSpaces = 1 + i;
+                var leadingSpaces = string.Join("", Enumerable.Repeat(" ", numLeadingSpaces));
+                Console.WriteLine($"{leadingSpaces}" + string.Join(" ", sequences[i]));
+            }
+
+            
+            var sum = 0;
+            foreach (var sequence in sequences)
+            {
+                sum += sequence.Last();
+            }
+            return sum;
+        }
+
+
+        private static int ComputeNextValueP2(List<int> values)
+        {
+            var sequences = new List<List<int>>
+            {
+                values
+            };
+            var newSequence = new List<int>();
+            var currSequence = values;
+            while (newSequence.Count == 0 || !newSequence.All(s => s == 0))
+            {
+                newSequence = [];
                 for (int i = 0; i < currSequence.Count - 1; i++)
                 {
                     newSequence.Add(currSequence[i + 1] - currSequence[i]);
@@ -56,12 +102,29 @@ namespace AoC2023.Day9
                 currSequence = newSequence;
             }
 
-            var sum = 0;
-            foreach (var sequence in sequences)
+            var lastSequence = sequences.Last();
+            //backfill last row with a 0
+            lastSequence.Insert(0, 0);
+
+            for (int i = sequences.Count - 2; i >= 0; i--)
             {
-                sum += sequence.Last();
+                var seqToUpdate = sequences[i];
+                var seqToLookAt = sequences[i + 1];
+                var valToInsert = seqToUpdate[0] - seqToLookAt[0];
+                seqToUpdate.Insert(0, valToInsert);
+                Console.WriteLine($"Inserting {valToInsert} at index 0 of sequence {i}");
             }
-            return sum;
+
+            Console.WriteLine("\n\n\n");
+            for (int i = 0; i < sequences.Count; i++)
+            {
+                var numLeadingSpaces = i;
+                var leadingSpaces = string.Join("", Enumerable.Repeat(" ", numLeadingSpaces));
+                Console.WriteLine($"{leadingSpaces}" + string.Join(" ", sequences[i]));
+            }
+
+
+            return sequences[0][0];
         }
 
         #region lil classes
