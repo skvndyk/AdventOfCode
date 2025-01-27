e56cc7498c17167b3bc05b9c74d699da2c808774
Sarah <sarah.vandyke@dayforce.com>
2025-01-27 14:03:32 -0500

    day 9 p1 2023

--- AOC/AoC2023/Day9/Day9_Program.cs
+++ AOC/AoC2023/Day9/Day9_Program.cs
@@ -14,21 +14,22 @@ namespace AoC2023.Day9
             var exInputStrings = Common.Utilities.ReadFileToStrings(exFilePath1);
             var inputStrings = Common.Utilities.ReadFileToStrings(filePath);
 
-            Console.WriteLine($"Part 1 example answer: {Part1(exInputStrings)}");
+            //Console.WriteLine($"Part 1 example answer: {Part1(exInputStrings)}");
             //Console.WriteLine($"Part 2 example answer: {Part2(exInputStrings)}");
 
-            //Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
+            Console.WriteLine($"Part 1 answer: {Part1(inputStrings)}");
             //Console.WriteLine($"Part 2 answer: {Part2(inputStrings)}");
         }
 
         private static int Part1(List<string> inputStrings)
         {
             var bloop = ParseInputStrings(inputStrings);
+            var runningSum = 0;
             foreach (var item in bloop)
             {
-                ComputeNextValue(item);
+                runningSum += ComputeNextValue(item);
             }
-            return 0;
+            return runningSum;
 
         }
 
@@ -41,20 +42,26 @@ namespace AoC2023.Day9
         {
             var sequences = new List<List<int>>();
             sequences.Add(values);
-
-            var sequence = new List<int>();
-            while (!sequence.Any() || !sequence.All(s => s == sequence[0]))
+            var newSequence = new List<int>();
+            var currSequence = values;
+            while (!newSequence.Any() || !newSequence.All(s => s == newSequence[0]))
             {
-                sequence = new List<int>();
-                for (int i = 0; i < values.Count - 1; i++)
+                newSequence = new List<int>();
+                for (int i = 0; i < currSequence.Count - 1; i++)
                 {
-                    sequence.Add(values[i + 1] - values[i]);
+                    newSequence.Add(currSequence[i + 1] - currSequence[i]);
                 }
 
-                sequences.Add(sequence);
+                sequences.Add(newSequence);
+                currSequence = newSequence;
             }
 
-            return 0;
+            var sum = 0;
+            foreach (var sequence in sequences)
+            {
+                sum += sequence.Last();
+            }
+            return sum;
         }
 
         #region lil classes
